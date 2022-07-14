f0c9fa5a692a35852343db46e2b0bbff99e485bc
Sadhana Lolla <sadhana@mit.edu>
2022-07-14 01:34:52 -0700

    histograms with VAE support

--- capsa/histogram.py
+++ capsa/histogram.py
@@ -1,14 +1,15 @@
 from numpy import histogram
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
 from histogram_layer import HistogramLayer
+from VAE import VAEWrapper
+from utils.utils import duplicate_layer, get_decoder
 
 
 class HistogramWrapper(keras.Model):
-    def __init__(self, base_model, is_standalone=True, num_bins=5):
+    def __init__(self, base_model, is_standalone=True, num_bins=5, metric_wrapper=None):
         super(HistogramWrapper, self).__init__()
-
+        self.base_model = base_model
         self.metric_name = "HistogramWrapper"
         self.is_standalone = is_standalone
 
@@ -18,21 +19,33 @@ class HistogramWrapper(keras.Model):
             )
 
         last_layer = base_model.layers[-1]
-        config = last_layer.get_config()
-        weights = last_layer.get_weights()
-        output_layer = type(last_layer).from_config(config)
-        output_layer.build(last_layer.input_shape)
-        output_layer.set_weights(weights)
-
-        self.output_layer = output_layer  # duplicate last layer
+        self.output_layer = duplicate_layer(last_layer)  # duplicate last layer
         self.histogram_layer = HistogramLayer(num_bins=num_bins)
 
+        self.metric_wrapper = metric_wrapper
+
+    def compile(self, optimizer, loss):
+        super(HistogramWrapper, self).compile(optimizer=optimizer, loss=loss)
+        if self.metric_wrapper is not None:
+            self.metric_wrapper = self.metric_wrapper(
+                base_model=self.base_model, is_standalone=self.is_standalone,
+            )
+            self.metric_wrapper.compile(optimizer=optimizer, loss=loss)
+            self.output_layer = self.metric_wrapper.output_layer
+
     def loss_fn(self, x, y, extractor_out=None):
-        if self.is_standalone:
+        if extractor_out is None:
             extractor_out = self.feature_extractor(x, training=True)
 
+        hist_input = extractor_out
+        if self.metric_wrapper is not None:
+            hist_input = self.metric_wrapper.input_to_histogram(
+                extractor_out, training=True
+            )
+            loss = self.metric_wrapper.loss_fn(x, y, extractor_out)
+
+        self.histogram_layer(hist_input)
         out = self.output_layer(extractor_out)
-        self.histogram_layer(extractor_out)
         loss = tf.reduce_mean(
             self.compiled_loss(y, out, regularization_losses=self.losses),
         )
@@ -65,7 +78,13 @@ class HistogramWrapper(keras.Model):
         if self.is_standalone:
             extractor_out = self.feature_extractor(x, training=False)
 
+        hist_input = extractor_out
+        if self.metric_wrapper is not None:
+            hist_input = self.metric_wrapper.input_to_histogram(
+                extractor_out, training=False
+            )
+
         predictor_y = self.output_layer(extractor_out)
-        bias = self.histogram_layer(extractor_out, training=False)
+        bias = self.histogram_layer(hist_input, training=False)
 
         return predictor_y, bias
