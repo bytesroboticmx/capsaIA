8e60054f26a8fae84073ac08f7bd3c230b6f1050
Sadhana Lolla <sadhana@mit.edu>
2022-07-14 23:02:19 -0700

    commented code

--- capsa/VAE.py
+++ capsa/VAE.py
@@ -6,7 +6,7 @@ from utils.utils import Sampling, duplicate_layer, reverse_model, _get_out_dim
 
 
 class VAEWrapper(keras.Model):
-    def __init__(self, base_model, is_standalone=True):
+    def __init__(self, base_model, is_standalone=True, decoder=None):
         super(VAEWrapper, self).__init__()
 
         self.metric_name = "VAEWrapper"
@@ -16,6 +16,7 @@ class VAEWrapper(keras.Model):
             base_model.inputs, base_model.layers[-2].output
         )
 
+        # Add layers for the mean and variance of the latent space
         latent_dim = _get_out_dim(self.feature_extractor)
         self.mean_layer = tf.keras.layers.Dense(latent_dim[-1])
         self.log_std_layer = tf.keras.layers.Dense(latent_dim[-1])
@@ -24,14 +25,26 @@ class VAEWrapper(keras.Model):
         last_layer = base_model.layers[-1]
         self.output_layer = duplicate_layer(last_layer)  # duplicate last layer
 
-        self.decoder = reverse_model(self.feature_extractor, latent_dim=latent_dim)
+        # Reverse model if we can, accept user decoder if we cannot
+        if (
+            type(self.feature_extractor) == tf.keras.Functional
+            or type(self.feature_extractor) == tf.keras.Sequential
+        ):
+            self.decoder = reverse_model(self.feature_extractor, latent_dim=latent_dim)
+        else:
+            if decoder is None:
+                raise ValueError(
+                    "If you provide a subclassed model, the decoder must also be specified"
+                )
+            else:
+                self.decoder = decoder
 
     def reconstruction_loss(self, mu, log_std, x):
-
+        # Calculates the VAE reconstruction loss by sampling and then feeding the latent vector through the decoder.
         sampled_latent_vector = self.sampling_layer([mu, log_std])
-
-        # reconstruction loss:
         reconstruction = self.decoder(sampled_latent_vector)
+
+        # Use decoder's reconstruction to compute loss
         mse_loss = tf.reduce_mean(
             tf.reduce_sum(tf.math.square(reconstruction - x), axis=-1)
         )
@@ -91,6 +104,7 @@ class VAEWrapper(keras.Model):
         return out, self.reconstruction_loss(mu, log_std, x)
 
     def input_to_histogram(self, extractor_out, training=None):
+        # Needed to interface with the Histogram metric.
         mu = self.mean_layer(extractor_out, training=training)
         log_std = self.log_std_layer(extractor_out, training=training)
         out = self.output_layer(extractor_out, training=training)
