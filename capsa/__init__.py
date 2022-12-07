7a150293ab52cf79658e2320a3cc71abeab2d29f
Ege Demir <egesko19@gmail.com>
2022-12-07 12:23:52 -0600

    Brought ed-notebooks into same histogram version as ed-histogram-updates

--- capsa/__init__.py
+++ capsa/__init__.py
@@ -4,6 +4,6 @@ from .base_wrapper import BaseWrapper
 from .risk_tensor import RiskTensor
 from .wrap import wrap
 
-from .bias import HistogramWrapper, HistogramCallback
+from .bias import HistogramWrapper
 from .aleatoric import MVEWrapper
 from .epistemic import DropoutWrapper, EnsembleWrapper, VAEWrapper
